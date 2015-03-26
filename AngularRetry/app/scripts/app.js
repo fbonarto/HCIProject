18d26111d95c654f3939e6bdcdeac3c8f5bdb7ff
unknown <cphazen@ufl.edu>
2015-03-26 01:23:34 -0400

    Routing!!

--- AngularRetry/app/scripts/app.js
+++ AngularRetry/app/scripts/app.js
@@ -1,3 +1,16 @@
 'use strict';
 
-angular.module('markticle', []);
+var app = angular.module('markticle', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/', {
+			templateUrl: 'views/search.html'
+		}).
+		when('/add', {
+			templateUrl: 'views/add.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+}]);
