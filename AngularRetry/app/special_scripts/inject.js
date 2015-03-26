8397a715b60a220c3c760a68baadfa9de6d2c78f
Courtney Hazen <cphazen@ufl.edu>
2015-03-26 00:19:33 -0400

    Update inject.js

--- AngularRetry/app/special_scripts/inject.js
+++ AngularRetry/app/special_scripts/inject.js
@@ -9,11 +9,29 @@ $(document).ready(function() {
 	$(document).on('click', '#markticle_button', function() {
 		var title = document.title;
 		var url = window.location.href;
+		var tags = [];
+		var description = "";
+		var color = ""; // should include # if hex
+		var screenshot = "";
+		var icon = "http://www.google.com/s2/favicons?domain=" + window.location.href;
+		var date = new Date();
+		var group = "";
 		chrome.extension.sendMessage({
 		   	action : 'add',
 		   	data: {
 				title: title,
-				url: url
+				url: url,
+				tags: tags;
+				description: description;
+				notes: notes;
+				color: color;
+				screenshot: screenshot;
+				icon: icon;
+				dateAdded: date;
+				dateVisited: date;
+				visited: 0;
+				order: 0;
+				group: group;
 			}
 		});
 
